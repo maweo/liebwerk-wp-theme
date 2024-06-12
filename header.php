bddf0d7dded85b8ecb81d3283812118e5e936866
lnrfl <38028633+lnrfl@users.noreply.github.com>
2024-06-12 10:38:29 +0200

    feature/hero

--- header.php
+++ header.php
@@ -6,6 +6,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php wp_head(); ?>
+
+  <!-- Proxima Nova Font -->
+  <link rel="stylesheet" href="https://use.typekit.net/wmy5aqp.css">
 </head>
 
 <body <?php body_class(); ?>>
@@ -13,7 +16,7 @@
   <?php maweo_navbar_before(); ?>
 
   <?php
-  include_once('template-parts/nav.php');
+  include_once ('template-parts/nav.php');
   ?>
 
   <?php maweo_navbar_after(); ?>
