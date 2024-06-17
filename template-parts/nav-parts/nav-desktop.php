2a4118ac1c7ad418ad34178588b4120875cd2998
lnrfl <38028633+lnrfl@users.noreply.github.com>
2024-06-17 12:28:47 +0200

    feat/language-switch

--- template-parts/nav-parts/nav-desktop.php
+++ template-parts/nav-parts/nav-desktop.php
@@ -70,24 +70,42 @@
         </div>
         <div class="nav-desktop__actions">
             <a href="<?php echo wc_get_cart_url(); ?>" class="nav-desktop__cart-link">
-                <img 
-                    src="<?php echo get_stylesheet_directory_uri() . '/assets/icons/cart.svg' ?>"
-                    alt="Cart Icon"
-                    class="nav-desktop__actions-icon"
-                />
+                <img src="<?php echo get_stylesheet_directory_uri() . '/assets/icons/cart.svg' ?>" alt="Cart Icon"
+                    class="nav-desktop__actions-icon" />
             </a>
             <a class="nav-desktop__cart-link" href="<?php echo wc_get_page_permalink('myaccount') ?>">
-                <img 
-                    src="<?php echo get_stylesheet_directory_uri() . '/assets/icons/user.svg' ?>"
-                    alt="Account Icon"
-                    class="nav-desktop__actions-icon"
-                />
+                <img src="<?php echo get_stylesheet_directory_uri() . '/assets/icons/user.svg' ?>" alt="Account Icon"
+                    class="nav-desktop__actions-icon" />
             </a>
-            <?php if($show_language_switcher): ?>
-                <label htmlFor="language-btn" class="nav-desktop__menu-button language-btn">
-                    <i class="bi bi-translate"></i>
-                </label>
+
+            <?php if ($show_language_switcher): ?>
+                <?php if (function_exists('pll_the_languages')): ?>
+                    <div class="language-switch">
+                        <div class="dropdown">
+                            <button class="btn dropdown-toggle" type="button" id="languageDropdown" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                <?php
+                                $current_locale = pll_current_language('locale');
+                                $current_lang_code = substr($current_locale, 0, 2); // Extract the first two characters
+                                echo strtolower(esc_html($current_lang_code)); // Convert to lowercase for display
+                                ?>
+                            </button>
+                            <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="languageDropdown">
+                                <?php
+                                $languages = pll_the_languages(array('dropdown' => 0, 'raw' => 1));
+                                foreach ($languages as $lang): ?>
+                                    <li>
+                                        <a class="dropdown-item" href="<?php echo esc_url($lang['url']); ?>">
+                                            <?php echo esc_html($lang['name']); ?>
+                                        </a>
+                                    </li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </div>
+                    </div>
+                <?php endif; ?>
             <?php endif; ?>
+
         </div>
     </div>
 </nav>
--- template-parts/nav-parts/nav-mobile.php
+++ template-parts/nav-parts/nav-mobile.php
@@ -106,26 +106,45 @@
                 </div>
             </ul>
 
+
             <?php if ($show_language_switcher): ?>
-                <label for="language-btn" class="nav-mobile__menu-button language-btn">
-                    <i class="bi bi-translate"></i>
-                </label>
+                <?php if (function_exists('pll_the_languages')): ?>
+                    <div class="language-switch">
+                        <div class="dropdown">
+                            <button class="btn dropdown-toggle" type="button" id="languageDropdown" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                <?php
+                                $current_locale = pll_current_language('locale');
+                                $current_lang_code = substr($current_locale, 0, 2); // Extract the first two characters
+                                echo strtolower(esc_html($current_lang_code)); // Convert to uppercase for display
+                                ?>
+                            </button>
+                            <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="languageDropdown">
+                                <?php
+                                $languages = pll_the_languages(array('dropdown' => 0, 'raw' => 1));
+                                foreach ($languages as $lang): ?>
+                                    <li>
+                                        <a class="dropdown-item" href="<?php echo esc_url($lang['url']); ?>">
+                                            <?php echo esc_html($lang['name']); ?>
+                                        </a>
+                                    </li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </div>
+                    </div>
+                <?php endif; ?>
             <?php endif; ?>
+
+
 
             <div class="nav-mobile__actions">
                 <a href="<?php echo wc_get_cart_url(); ?>" class="nav-mobile__actions-link">
-                    <img 
-                        src="<?php echo get_stylesheet_directory_uri() . '/assets/icons/cart.svg' ?>"
-                        alt="Cart Icon"
-                        class="nav-mobile__actions-icon"
-                    />
+                    <img src="<?php echo get_stylesheet_directory_uri() . '/assets/icons/cart.svg' ?>" alt="Cart Icon"
+                        class="nav-mobile__actions-icon" />
                 </a>
                 <a href="<?php echo wc_get_page_permalink('myaccount') ?>" class="nav-mobile__actions-link">
-                    <img 
-                        src="<?php echo get_stylesheet_directory_uri() . '/assets/icons/user.svg' ?>"
-                        alt="Account Icon"
-                        class="nav-mobile__actions-icon"
-                    />
+                    <img src="<?php echo get_stylesheet_directory_uri() . '/assets/icons/user.svg' ?>"
+                        alt="Account Icon" class="nav-mobile__actions-icon" />
                 </a>
             </div>
 
