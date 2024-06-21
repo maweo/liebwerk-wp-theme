631ddc901bd0dabdcd07e72cdef2a2b1995fa72a
lnrfl <38028633+lnrfl@users.noreply.github.com>
2024-06-21 09:21:26 +0200

    fix/update topbar

--- template-parts/nav-parts/nav-mobile.php
+++ template-parts/nav-parts/nav-mobile.php
@@ -106,36 +106,9 @@
                 </div>
             </ul>
 
-
             <?php if ($show_language_switcher): ?>
-                <?php if (function_exists('pll_the_languages')): ?>
-                    <div class="language-switch">
-                        <div class="dropdown">
-                            <button class="btn dropdown-toggle" type="button" id="languageDropdown" data-bs-toggle="dropdown"
-                                aria-expanded="false">
-                                <?php
-                                $current_locale = pll_current_language('locale');
-                                $current_lang_code = substr($current_locale, 0, 2); // Extract the first two characters
-                                echo strtolower(esc_html($current_lang_code)); // Convert to uppercase for display
-                                ?>
-                            </button>
-                            <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="languageDropdown">
-                                <?php
-                                $languages = pll_the_languages(array('dropdown' => 0, 'raw' => 1));
-                                foreach ($languages as $lang): ?>
-                                    <li>
-                                        <a class="dropdown-item" href="<?php echo esc_url($lang['url']); ?>">
-                                            <?php echo esc_html($lang['name']); ?>
-                                        </a>
-                                    </li>
-                                <?php endforeach; ?>
-                            </ul>
-                        </div>
-                    </div>
-                <?php endif; ?>
+                <?php include (get_stylesheet_directory() . '/template-parts/nav-parts/language-switch.php') ?>
             <?php endif; ?>
-
-
 
             <div class="nav-mobile__actions">
                 <a href="<?php echo wc_get_cart_url(); ?>" class="nav-mobile__actions-link">
