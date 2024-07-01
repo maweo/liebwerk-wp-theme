3ecad1abb44081fbfc69060adafa4ad7d410b788
Dominik <hi@dominik-schratl.at>
2024-07-01 13:18:27 +0200

    feat: Slider wotking

--- js/global.js
+++ js/global.js
@@ -3,9 +3,10 @@ import { copyElement } from './sections/logo-repeater';
 import { backToTop } from './sections/back-to-top';
 import { initPostGrid } from './sections/post-grid';
 import { initTopbar } from './sections/topbar';
+import { initTimeline } from './sections/timeline';
 
 initNav();
 copyElement();
 backToTop();
 initPostGrid();
-initTopbar();
+initTimeline();
