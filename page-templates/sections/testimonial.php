4c6da8967b961fa9125e3a8d09c0a512c8620c8a
valentinadmaweo <103988622+valentinadmaweo@users.noreply.github.com>
2024-07-02 14:29:17 +0200

    feat: added carousel (not finished yet)

--- page-templates/sections/testimonial.php
+++ page-templates/sections/testimonial.php
@@ -4,6 +4,7 @@
     $heading_tag = get_sub_field('heading_tag');
     $text = get_sub_field('text');
     $testimonials = get_sub_field('testimonials');
+    $carousel_id = "testimonialCarousel" . uniqid();
 ?>
 
 <section class="testimonials">
@@ -20,26 +21,55 @@
                 <div class="row justify-content-center">
                     <?php echo maweo_get_heading($heading, $heading_tag, "testimonials__heading text-center col-12 col-lg-8") ?>
                 </div>
-                
                 <?php if($testimonials): ?>
-                    <div class="testimonials__row">
-                        <?php foreach($testimonials as $index => $testimonial) : ?>
-                            <?php 
-                                $author = get_field('author', $testimonial['testimonial']);
-                                $text = get_field('text', $testimonial['testimonial']);
-                            ?>
-                            <div class="col-12 col-lg-4 testimonials__item testimonials__item">
-                                <div class="maweo-wysiwyg testimonials__text">
-                                    <?php echo $text?>
-                                </div>
-                                <?php if($author): ?>
-                                    <div class="testimonials__author">
-                                        <?php echo $author?>
+                    <div id="<?php echo $carousel_id ?>" class="carousel slide testimonials__carousel" data-bs-ride="carousel">
+                        <div class="carousel-inner testimonials__item-wrapper row">
+                            <?php foreach($testimonials as $index => $testimonial) : ?>
+                                <?php 
+                                    $author = get_field('author', $testimonial['testimonial']);
+                                    $text = get_field('text', $testimonial['testimonial']);
+                                    $class = $index == 0 ? "active" : "";
+                                ?>
+                                <div class="col-12 col-lg-4 carousel-item testimonials__item testimonials__item--<?php echo $background ?> <?php echo $class ?>">
+                                    <div class="maweo-wysiwyg testimonials__text">
+                                        <?php echo $text?>
                                     </div>
-                                <?php endif; ?>
+                                    <?php if($author): ?>
+                                        <div class="testimonials__author">
+                                            <?php echo $author?>
+                                        </div>
+                                    <?php endif; ?>
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+                        <div class="row justify-content-betweeen">
+                            <div class="carousel-indicators testimonials__indicators">
+                                <?php foreach($testimonials as $index => $testimonial): ?>
+                                    <?php $class = $index == 0 ? "active testimonials__indicator--active" : ""; ?>
+                                    <button 
+                                        type="button" 
+                                        data-bs-target="#<?php echo $carousel_id?>" 
+                                        data-bs-slide-to="<?php echo $index?>" 
+                                        aria-label="Slide <?php echo $index?>"
+                                        class="testimonials__indicator <?php echo $class?>"
+                                        <?php if($index == 0): ?>
+                                            aria-current="true" 
+                                        <?php endif; ?>
+                                    ></button>
+                                <?php endforeach; ?>
                             </div>
-                        <?php endforeach; ?>
-                    </div>
+                            <div class="width-auto">
+                                <button class="carousel-control-prev testimonials__control testimonials__control--prev" type="button" data-bs-target="#<?php echo $carousel_id?>" data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon testimonials__prev-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button class="carousel-control-next testimonials__control testimonials__control--next" type="button" data-bs-target="#<?php echo $carousel_id?>" data-bs-slide="next">
+                                    <span class="carousel-control-next-icon testimonials__next-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Next</span>
+                                </button>
+                            </div>
+                        </div>
+                        
                     </div>
                 <?php endif; ?>
             </div>
