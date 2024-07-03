8a37c6d58d753af35b1ff7823d39393cd53610f0
lnrfl <38028633+lnrfl@users.noreply.github.com>
2024-07-03 09:29:14 +0200

    feat/image-text fit option

--- page-templates/sections/image_text_default.php
+++ page-templates/sections/image_text_default.php
@@ -9,6 +9,7 @@ $link = get_sub_field('link');
 
 $image = get_image_data_from_sub_field('image');
 $image_position = get_sub_field('image_position');
+$image_fit = get_sub_field('image_fit') ?? "cover";
 
 $is_centered_mobile_class = get_sub_field('is_text_centered_mobile') ? " d-block mx-auto mx-lg-0 text-center text-lg-start" : "";
 ?>
@@ -17,12 +18,12 @@ $is_centered_mobile_class = get_sub_field('is_text_centered_mobile') ? " d-block
 
 <section class="image-text">
     <div class="container">
-        <div class="row">
+        <div class="row align-items-stretch">
             <div
-                class="col-12 col-lg-5 order-1 <?php echo $image_position === "right" ? "order-lg-0" : "order-lg-2 offset-lg-1 " ?>">
+                class="col-12 col-lg-5 order-1 d-flex flex-column justify-content-center <?php echo $image_position === "right" ? "order-lg-0" : "order-lg-2 offset-lg-1" ?>">
                 <div class="image-text__content">
                     <?php if ($heading_icon && $heading_icon['url'] != ""): ?>
-                        <img alt="<?php $heading_icon['alt'] ?>"
+                        <img alt="<?php echo $heading_icon['alt'] ?>"
                             class="image-text__icon <?php echo $is_centered_mobile_class ?>"
                             src="<?php echo $heading_icon['url'] ?>">
                     <?php endif; ?>
@@ -40,8 +41,12 @@ $is_centered_mobile_class = get_sub_field('is_text_centered_mobile') ? " d-block
                     <?php endif; ?>
                 </div>
             </div>
-            <div class="col-12 col-lg-6 order-lg-1 <?php echo $image_position === "right" ? "offset-lg-1" : "" ?>">
-                <img src="<?php echo $image['url'] ?>" alt="<?php echo $image['alt'] ?>" class="image-text__image">
+            <div
+                class="col-12 col-lg-6 order-lg-1 d-flex align-items-center <?php echo $image_position === "right" ? "offset-lg-1" : "" ?>">
+                <div class="w-100">
+                    <img src="<?php echo $image['url'] ?>" alt="<?php echo $image['alt'] ?>"
+                        class="image-text__image image-text__image--<?php echo $image_fit ?>">
+                </div>
             </div>
         </div>
     </div>
