447fdfd775d3a3bbec5bc025e42258c0e102efa1
lnrfl <38028633+lnrfl@users.noreply.github.com>
2024-07-03 13:53:59 +0200

    fix/update image text options

--- page-templates/sections/image_text_default.php
+++ page-templates/sections/image_text_default.php
@@ -7,11 +7,13 @@ $sub_heading = get_sub_field('sub_heading');
 $text = get_sub_field('text');
 $link = get_sub_field('link');
 
-$number_of_images = get_sub_field('number_of_images');
-$image = get_image_data_from_sub_field('image');
-if($number_of_images == 'two') {
+$image_one = get_image_data_from_sub_field('image');
+
+if (get_sub_field('image_two')) {
     $image_two = get_image_data_from_sub_field('image_two');
-};
+} else {
+    $image_two = null;
+}
 
 $image_position = get_sub_field('image_position');
 $image_fit = get_sub_field('image_fit') ?? "cover";
@@ -23,10 +25,10 @@ $is_centered_mobile_class = get_sub_field('is_text_centered_mobile') ? " d-block
 
 <section class="image-text">
     <div class="container">
-        <div class="row align-items-stretch">
+        <div class="row">
             <div
-                class="col-12 col-lg-6 order-1 <?php echo $number_of_images === "two" ? "order-0" : "" ?> <?php echo $image_position === "right" ? "order-lg-0" : "order-1 order-lg-2 offset-lg-1 " ?> ">
-                <div class="image-text__content">
+                class="col-12 <?php echo $image_two ? "col-lg-5" : "col-lg-6" ?> order-1 d-flex flex-column justify-content-center <?php echo $image_position === "right" ? "order-lg-0" : "order-1 order-lg-2 offset-lg-1 " ?> ">
+                <div class="image-text__content mt-3 mt-lg-0">
                     <?php if ($heading_icon && $heading_icon['url'] != ""): ?>
                         <img alt="<?php echo $heading_icon['alt'] ?>"
                             class="image-text__icon <?php echo $is_centered_mobile_class ?>"
@@ -46,11 +48,20 @@ $is_centered_mobile_class = get_sub_field('is_text_centered_mobile') ? " d-block
                     <?php endif; ?>
                 </div>
             </div>
-            <div class="col-12 col-lg-5 mb-sm-5 mb-lg-0 order-lg-1 <?php echo $image_position === "right" ? "offset-lg-1" : "" ?>">
-                <div class="image-text__image-wrapper <?php echo $number_of_images === "two" ? "col-12 col-md-6" : "" ?>">
-                    <img src="<?php echo $image['url'] ?>" alt="<?php echo $image['alt'] ?>" class="image-text__image <?php echo $number_of_images === "two" ? "image-text__image-height" : "" ?> ">
-                    <?php if ($image_two): ?>
-                        <img src="<?php echo $image_two['url'] ?>" alt="<?php echo $image_two['alt'] ?>" class="image-text__image ">
+            <div
+                class="col-12 <?php echo $image_two ? "col-lg-6" : "col-lg-5" ?> order-lg-1 <?php echo $image_position === "right" ? "offset-lg-1" : "" ?>">
+                <div class="image-text__image-wrapper">
+                    <?php if ($image_one && $image_one['url'] != ""): ?>
+                        <div>
+                            <img src="<?php echo $image_one['url'] ?>" alt="<?php echo $image_one['alt'] ?>"
+                                class="image-text__image image-text__image--<?php echo $image_fit ?>">
+                        </div>
+                    <?php endif; ?>
+                    <?php if ($image_two && $image_two['url'] != ""): ?>
+                        <div>
+                            <img src="<?php echo $image_two['url'] ?>" alt="<?php echo $image_two['alt'] ?>"
+                                class="image-text__image image-text__image--<?php echo $image_fit ?>">
+                        </div>
                     <?php endif; ?>
                 </div>
             </div>
