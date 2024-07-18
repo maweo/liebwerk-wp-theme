37e4e96811f0630552cacbc2edb6fd672de42365
lnrfl <38028633+lnrfl@users.noreply.github.com>
2024-07-18 13:54:27 +0200

    feat/update-footer

--- footer.php
+++ footer.php
@@ -12,27 +12,36 @@ maweo_footer_before();
 ?>
 
 <?php
-include_once('template-parts/back-to-top.php');
+include_once ('template-parts/back-to-top.php');
 ?>
 
 <footer id="site-footer" class="footer">
   <div class="container">
     <div class="row">
       <div class="col-12 col-lg-6">
-        <?php if ($footer_logo && $footer_logo['url'] != "") : ?>
+        <?php if ($footer_logo && $footer_logo['url'] != ""): ?>
           <img src="<?php echo $footer_logo['url'] ?>" alt="<?php echo $footer_logo['alt'] ?>" class="footer__logo">
+        <?php endif; ?>
+        <?php if ($footer_social_links): ?>
+          <div class="footer__social-links">
+            <?php foreach ($footer_social_links as $link): ?>
+              <?php if ($link['link']): ?>
+                <?php echo maweo_get_icon_link($link['link'], $link['icon'], "footer__social-link", "footer__social-link-icon") ?>
+              <?php endif; ?>
+            <?php endforeach; ?>
+          </div>
         <?php endif; ?>
       </div>
       <div class="col-12 col-lg-2">
-        <?php if ($footer_column_1['heading']) : ?>
+        <?php if ($footer_column_1['heading']): ?>
           <h2 class="footer__heading">
             <?php echo $footer_column_1['heading'] ?>
           </h2>
         <?php endif; ?>
-        <?php if ($footer_column_1['links']) : ?>
+        <?php if ($footer_column_1['links']): ?>
           <div class="footer__links">
-            <?php foreach ($footer_column_1['links'] as $link) : ?>
-              <?php if ($link['link']) : ?>
+            <?php foreach ($footer_column_1['links'] as $link): ?>
+              <?php if ($link['link']): ?>
                 <?php echo maweo_get_link($link['link'], "footer__link") ?>
               <?php endif; ?>
             <?php endforeach; ?>
@@ -40,15 +49,15 @@ include_once('template-parts/back-to-top.php');
         <?php endif; ?>
       </div>
       <div class="col-12 col-lg-2">
-        <?php if ($footer_column_2['heading']) : ?>
+        <?php if ($footer_column_2['heading']): ?>
           <h2 class="footer__heading">
             <?php echo $footer_column_2['heading'] ?>
           </h2>
         <?php endif; ?>
-        <?php if ($footer_column_2['links']) : ?>
+        <?php if ($footer_column_2['links']): ?>
           <div class="footer__links">
-            <?php foreach ($footer_column_2['links'] as $link) : ?>
-              <?php if ($link['link']) : ?>
+            <?php foreach ($footer_column_2['links'] as $link): ?>
+              <?php if ($link['link']): ?>
                 <?php echo maweo_get_link($link['link'], "footer__link") ?>
               <?php endif; ?>
             <?php endforeach; ?>
@@ -56,15 +65,15 @@ include_once('template-parts/back-to-top.php');
         <?php endif; ?>
       </div>
       <div class="col-12 col-lg-2">
-        <?php if ($footer_column_3['heading']) : ?>
+        <?php if ($footer_column_3['heading']): ?>
           <h2 class="footer__heading">
             <?php echo $footer_column_3['heading'] ?>
           </h2>
         <?php endif; ?>
-        <?php if ($footer_column_3['links']) : ?>
+        <?php if ($footer_column_3['links']): ?>
           <div class="footer__links">
-            <?php foreach ($footer_column_3['links'] as $link) : ?>
-              <?php if ($link['link']) : ?>
+            <?php foreach ($footer_column_3['links'] as $link): ?>
+              <?php if ($link['link']): ?>
                 <?php echo maweo_get_link($link['link'], "footer__link") ?>
               <?php endif; ?>
             <?php endforeach; ?>
@@ -78,34 +87,22 @@ include_once('template-parts/back-to-top.php');
   <div class="container">
     <div class="row">
       <div class="col-12 col-lg-5 order-1 order-lg-1">
-        <?php if ($footer_legal_links) : ?>
+        <?php if ($footer_legal_links): ?>
           <div class="bottom-bar__links">
-            <?php foreach ($footer_legal_links as $link) : ?>
-              <?php if ($link['link']) : ?>
+            <?php foreach ($footer_legal_links as $link): ?>
+              <?php if ($link['link']): ?>
                 <?php echo maweo_get_link($link['link'], "bottom-bar__link") ?>
               <?php endif; ?>
             <?php endforeach; ?>
           </div>
         <?php endif; ?>
       </div>
-      <div class="col-12 col-lg-5 order-2 order-lg-1">
-        <?php if ($footer_icons) : ?>
+      <div class="col-12 col-lg-7 bottom-bar__company order-0 order-lg-1">
+        <?php if ($footer_icons): ?>
           <div class="bottom-bar__icons">
-            <?php foreach ($footer_icons as $image) : ?>
-              <?php $image = get_image_data($image['icon']);?>
+            <?php foreach ($footer_icons as $image): ?>
+              <?php $image = get_image_data($image['icon']); ?>
               <img src="<?php echo $image['url'] ?>" alt="<?php echo $image['alt'] ?>" class="bottom-bar__icon">
-  
-            <?php endforeach; ?>
-          </div>
-        <?php endif; ?>
-      </div>
-      <div class="col-12 col-lg-2 bottom-bar__company order-0 order-lg-1">
-        <?php if ($footer_social_links) : ?>
-          <div class="footer__social-links">
-            <?php foreach ($footer_social_links as $link) : ?>
-              <?php if ($link['link']) : ?>
-                <?php echo maweo_get_icon_link($link['link'], $link['icon'], "footer__social-link", "footer__social-link-icon") ?>
-              <?php endif; ?>
             <?php endforeach; ?>
           </div>
         <?php endif; ?>
